c09b2698230e101502a25ef1b983875e0bc89743
pedro.neres <showmediapublicidade@gmail.com>
2023-05-09 15:52:42 -0300

    ajustando

---
+++ pages/chat.php
@@ -0,0 +1,20 @@
+<div class="box-message">
+                    <span class="span" onclick="fecharmessage();">X</span>
+                    <div class="head"><div class="img-contact"></div><span>Gileade Despachante</span></div>
+                    <div id="body-message" class="body">
+                        <ul id="lista-message">
+                            <li class="pergunta">Olá! Eu posso te ajudar?</li>
+                        </ul>
+
+                        <button id="preciso" onclick="eupreciso();">Eu preciso!</button>
+                    </div>
+                    <div class="footer">
+                        <input type="text" name="message" id="message">
+                        <button id="btn-send"><ion-icon name="send-outline"></ion-icon></button>
+                    </div>
+                </div>
+
+                <div class="contact-online">
+                    <div class="banner">Olá! Eu posso te ajudar?</div>
+                    <div class="img-contact"></div>
+                </div>
