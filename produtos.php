316b5bfda8c3c8970d4a70757a2e59cc3daaf5fc
Pedro Neres <carvalho.pedro.n@gmail.com>
2023-06-01 13:16:10 -0300

    atualizando

---
+++ produtos.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MJP Industrial</title>
+
+        <!-- FONTS DO GOOGLE-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+
+        <!-- CSS BOOTSTRAP -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <!-- CSS DA APLICAÇÃO-->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/mobile.css">
+		 
+    </head>
+
+    <body>
+           <?php 
+            include "pages/nav.php"; 
+           ?>
+
+            <main>
+
+            <section id="search-top" class="search-top col-12">
+                
+            <video autoplay muted loop>
+                <source src="img/mjp.mp4" type="video/mp4">
+            </video>
+            </section>
+           produtos
+
+                <?php 
+                    include "pages/chat.php";
+                ?>
+               
+            </main>
+           
+            <?php 
+                include "pages/footer.php"; 
+            ?>
+
+
+ 
+    <!-- script bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ 
+    <script src="js/jquery.mask.js"></script>
+      <!-- SCRIPT DA APLICAÇÃO-->
+      <script src="js/script.js?v=2"></script>
+    <!-- icons -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    </body>
+</html>
