51ba63719e0533e25f15e25fb2176601a01cc3ec
pedro.neres <showmediapublicidade@gmail.com>
2023-06-09 13:00:07 -0300

    adicionando video de cada produto

--- controllers/ProdutosController.php
+++ controllers/ProdutosController.php
@@ -25,8 +25,21 @@
                     <?php echo ucwords($produto) ?>
                 </h1>
 
-                <?php 
-                    if($idproduto == 3){?>
+                <?php if($idproduto == 1){ ?>
+                    <p>
+                    O Painel de LED Redondo é um produto inovador e versátil utilizado em diversos ambientes, como lojas, shoppings, eventos e espaços corporativos. Com um design arrojado e elegante, esse painel proporciona uma experiência visual imersiva e impactante para os espectadores.
+                    <br><br>
+                    Através do Painel de LED Redondo, é possível exibir conteúdos dinâmicos, como imagens em alta resolução, vídeos atrativos e informações relevantes de maneira envolvente. Esse formato circular cria uma atmosfera única, capturando a atenção do público e transmitindo mensagens de forma eficaz.
+                    <br><br>
+                    Para garantir a instalação adequada e segura do Painel de LED Redondo, a MJP Industrial oferece suportes de alta qualidade, especialmente projetados para esse tipo de painel. Nossos suportes são fabricados com materiais duráveis e resistentes, assegurando a fixação estável do painel em qualquer ambiente.
+                    <br><br>
+                    Além disso, a MJP Industrial se destaca pelo seu compromisso com a qualidade e o acabamento impecável. Nossos suportes são cuidadosamente projetados para oferecer suporte adequado ao Painel de LED Redondo, garantindo sua durabilidade e estabilidade ao longo do tempo.
+                    <br><br>
+                    Optar pelos suportes da MJP Industrial significa contar com um produto confiável e de alta performance, que proporcionará uma exibição impecável do seu Painel de LED Redondo. Invista na qualidade e no acabamento excepcional dos nossos suportes para garantir uma instalação segura e um visual impactante para o seu painel.
+                    </p>
+                <?php }else if($idproduto == 2){?>
+                    <p></p>
+                <?php }else if($idproduto == 3){?>
                         <p>
                         O Menu Board Digital é uma solução visual inovadora amplamente adotada em estabelecimentos do ramo alimentício, como restaurantes, redes de fast-food e lanchonetes. Essa ferramenta tem como objetivo principal realçar as opções de cardápio por meio de imagens, vídeos e informações sobre os produtos, de maneira cativante, dinâmica, prática e de fácil compreensão para os clientes.
                         <br><br>
@@ -38,7 +51,11 @@
                         <br><br>
                         Invista no Suporte de Menu Board Digital ou VideoWall da MJP Industrial e impulsione seu negócio com uma solução moderna, impactante e de alto desempenho, que atrairá e envolverá seus clientes de forma única.
                         </p>
-                    <?php }?>
+                <?php }else if($idproduto == 4){?>
+                    <p></p>
+                <?php }else if($idproduto == 5){?>
+                    <p></p>
+                <?php }?>
 
                     <br>
                         <a href="https://wa.me/5511941202161">Solicitar Cotação</a>
