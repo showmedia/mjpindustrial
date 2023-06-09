07c0abc45779ba4e81247e8630bbcb369e73f0ae
pedro.neres <showmediapublicidade@gmail.com>
2023-06-09 12:22:20 -0300

    adicionando video de cada produto

--- controllers/ProdutosController.php
+++ controllers/ProdutosController.php
@@ -22,12 +22,22 @@
                 <?php $produto = urldecode($route2); ?>
           
                 <h1>
-                    <?php echo ucwords($produto) .' - '.$idproduto; ?>
+                    <?php echo ucwords($produto) ?>
                 </h1>
 
                 <?php 
-                    if($route2 == 'paineis'){?>
-       
+                    if($idproduto == 1){?>
+                        <p>
+                        O Menu Board Digital é uma solução visual inovadora amplamente adotada em estabelecimentos do ramo alimentício, como restaurantes, redes de fast-food e lanchonetes. Essa ferramenta tem como objetivo principal realçar as opções de cardápio por meio de imagens, vídeos e informações sobre os produtos, de maneira cativante, dinâmica, prática e de fácil compreensão para os clientes.
+                        <br><br>
+                        Pesquisas revelam que mais de 50% das decisões de compra dos consumidores são tomadas diretamente no ponto de venda, tornando o Menu Board Digital um recurso de engajamento indispensável para sua estratégia de marketing.
+                        <br><br>
+                        Além disso, a implementação de um Menu Board Digital oferece vantagens adicionais, como a atração de novos clientes, que são impactados e influenciados diretamente pela tela ao passarem pelo local. Os produtos e preços podem ser atualizados de forma individual e segmentada por região, tornando seu menu mais interativo e atraente.
+                        <br><br>
+                        Ao optar pelos serviços da MJP Industrial para instalar seu Menu Board, você contará com suportes de alta qualidade e um material excelente, garantindo durabilidade e resistência. A MJP Industrial se destaca no mercado por fornecer produtos com acabamento impecável, que agregam valor ao ambiente e valorizam a apresentação do cardápio.
+                        <br><br>
+                        Invista no Suporte de Menu Board Digital ou VideoWall da MJP Industrial e impulsione seu negócio com uma solução moderna, impactante e de alto desempenho, que atrairá e envolverá seus clientes de forma única.
+                        </p>
                     <?php }?>
             </section>
           
